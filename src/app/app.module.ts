53b7434eb259d02af78781ee99df16a1cb18044d
Esjihn <sysnom@gmail.com>
2022-06-05 10:08:03 -0400

    Update new commerce app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProductService], // inject (Injectable) into other parts of our application.
   bootstrap: [AppComponent]
 })
 export class AppModule { }
